06955904cd61eb6880c1f35d156fad5966cb295d
tricore.dev20 <nikitabhatia@tricoreitsolutions.com>
2013-11-21 11:09:08 +0530

    uploading js folder in admin panel

---
+++ admin/js/script.js
@@ -0,0 +1,14 @@
+$(document).ready(function() 
+{
+
+	//Content boxes expand/collapse
+	$(".initial-expand").hide();
+
+	$("div.content-module-heading").click(function()
+	{
+		$(this).next("div.content-module-main").slideToggle();
+
+		$(this).children(".expand-collapse-text").toggle();
+	});
+	
+});
